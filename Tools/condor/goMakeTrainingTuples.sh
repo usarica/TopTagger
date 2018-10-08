8051a32186ff847d7e699307230d6216a27953ce
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-10-08 10:15:24 -0500

    New training vs 9

--- Tools/condor/goMakeTrainingTuples.sh
+++ Tools/condor/goMakeTrainingTuples.sh
@@ -30,8 +30,8 @@ ls -lhrt
 
 #mv trainingTuple_* ${_CONDOR_SCRATCH_DIR}
 for i in trainingTuple_*; do
-    echo xrdcp $i root://cmseos.fnal.gov//store/user/cmadrid/trainingTuples/BackGroundMVA_V8_CM_All_GoodJets/$i
-    xrdcp $i root://cmseos.fnal.gov//store/user/cmadrid/trainingTuples/BackGroundMVA_V8_CM_All_GoodJets/$i
+    echo xrdcp $i root://cmseos.fnal.gov//store/user/cmadrid/trainingTuples/BackGroundMVA_V9_CM_All_GoodJets_Inclusive/$i
+    xrdcp $i root://cmseos.fnal.gov//store/user/cmadrid/trainingTuples/BackGroundMVA_V9_CM_All_GoodJets_Inclusive/$i
     rm $i
 done
 
